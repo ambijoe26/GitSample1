9ddd2332f57d2b798fcd5e4ccf3fcf78f5519504
ambijoe26 <ambijoe26@gmail.com>
2022-09-07 21:41:47 +0530

    Ambrose Work Code

--- src/test/java/org/test/com/Sample1.java
+++ src/test/java/org/test/com/Sample1.java
@@ -5,6 +5,7 @@ public class Sample1 {
 		System.out.println("Monitom1");
 		System.out.println("Monitom2");
 		System.out.println("Monitom3");
+		System.out.println("Hello Monisha this is Ambrose");
 		
 	}
 
