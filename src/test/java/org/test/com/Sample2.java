3116c209fb2d424dd4422c217fa3f2b49fc57915
Monisha730520 <monitom234@gmail.com>
2022-09-07 21:50:02 +0530

    Chanes to code

--- src/test/java/org/test/com/Sample2.java
+++ src/test/java/org/test/com/Sample2.java
@@ -3,6 +3,8 @@ package org.test.com;
 public class Sample2 {
 	public static void main(String[] args) {
 		System.out.println("Hello This is Ambrose");
+		System.out.println("Hello This is Monisha");
+	}
 	}
 
 }
